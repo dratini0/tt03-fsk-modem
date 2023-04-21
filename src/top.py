5786a4958f9501e0f02946014b1b8648aa247b5d
Balint Kovacs <dratini0@gmail.com>
2023-04-21 02:24:24 +0100

    It doesn't fit.

--- src/top.py
+++ src/top.py
@@ -3,6 +3,9 @@
 from amaranth import *
 from amaranth.back import verilog
 
+from rx import Rx
+from wave_gen import WaveGen
+
 
 class FSKModemTop(Elaboratable):
     """The top level, responsible for pinout definition"""
@@ -11,16 +14,20 @@ class FSKModemTop(Elaboratable):
         self.io_in = Signal(8)
         self.io_out = Signal(8)
 
-        self._counter1 = Signal(signed(8))
-        self._counter2 = Signal(signed(8))
-        self._counter3 = Signal(signed(8))
-        self._counter4 = Signal(signed(8))
+        self._rx = Rx()
+        self._wg1 = WaveGen()
+        self._wg2 = WaveGen()
 
     def elaborate(self, platform):
         m = Module()
 
         clk_in = self.io_in[0]
         rst = self.io_in[1]
+        control_in = self.io_in[2]
+        samples_in = self.io_in[3:8]
+        data_out = self.io_out[0]
+        valid_out = self.io_out[1]
+        samples_out = self.io_out[3:8]
 
         # Set up clock domain from io_in[0]
         cd_sync = ClockDomain("sync")
@@ -28,13 +35,20 @@ class FSKModemTop(Elaboratable):
         m.d.comb += cd_sync.rst.eq(rst)
         m.domains += cd_sync
 
-        m.d.sync += [
-            self._counter1.eq(self._counter1 + 7),
-            self._counter2.eq(self._counter2 + 11),
-            self._counter3.eq(self._counter3 + 13),
-            self._counter4.eq(self._counter4 + 17),
+        m.submodules.rx = self._rx
+        m.submodules.wg1 = self._wg1
+        m.submodules.wg2 = self._wg2
+
+        m.d.comb += [
+            self._rx.frequency.eq(round(1750 / 12500 * 1024)),
+            self._wg1.frequency.eq(round(770 / 12500 * 1024)),
+            self._wg2.frequency.eq(round(1336 / 12500 * 1024)),
+            self._rx.frequency_invert.eq(0),
+            self._rx.in_.eq(samples_in),
+            data_out.eq(self._rx.out),
+            valid_out.eq(self._rx.valid),
+            samples_out.eq((self._wg1.out[-5:] + self._wg2.out[-5:])[1:]),
         ]
-        m.d.comb += self.io_out[0].eq((self._counter1 * self._counter2 >> 7) > (self._counter3 * self._counter4 >> 7))
 
         return m
 
